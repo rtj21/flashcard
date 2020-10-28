24ba21ed78fd89eb5a536d41e0422d8bed7ab75f
rtj21 <robert21tj@gmail.com>
2020-10-29 00:33:15 +0200

    Save card

--- script.js
+++ script.js
@@ -14,4 +14,18 @@ function saveFlashCard(){
     const question =document.getElementById("question").value;
     const answer =document.getElementById("answer").value;
 
+    var node0 = document.createElement("div");
+    var node1 = document.createElement("h2");
+    var node2 = document.createElement("h2");
+
+    node1.innerHTML=question;
+    node2.innerHTML=answer;
+    
+    node2.style.display="none";
+    node2.style.color="red";
+
+    node0.appendChild(node1);
+    node0.appendChild(node2);
+
+    container3.insertAdjacentElement("beforeend", node0);
 }
