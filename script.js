cc38661819fde47f9e538d80feb4e514a74d17c9
rtj21 <robert21tj@gmail.com>
2020-10-29 17:30:58 +0200

    Edit button

--- script.js
+++ script.js
@@ -11,8 +11,8 @@ function hideContainer2(){
 }
 
 function saveFlashCard(){
-    const question =document.getElementById("question").value;
-    const answer =document.getElementById("answer").value;
+    var question =document.getElementById("question").value;
+    var answer =document.getElementById("answer").value;
 
     var cardDiv = document.createElement("div");
     var questionDiv = document.createElement("h2");
@@ -20,24 +20,33 @@ function saveFlashCard(){
     var showButton = document.createElement("button");
     var editButton = document.createElement("button");
     var editTextArea = document.createElement("textarea");
+    var confirmEdit = document.createElement("button");
 
 
     questionDiv.innerHTML=question;
     answerDiv.innerHTML=answer;
 
     showButton.innerHTML="Show answer";
-    showButton.className="btn btn-outline-info btn-sm"
     editButton.innerHTML="Edit";
+    confirmEdit.innerHTML="Confirm edit";
+
+    showButton.className="btn btn-outline-info btn-sm"
     editButton.className="btn btn-outline-info btn-sm"
+    confirmEdit.className="btn btn-outline-danger btn-sm"
+
+    editTextArea.id="newText";
 
     answerDiv.style.display="none";
     answerDiv.style.color="red";
     editTextArea.style.display="none";
+    confirmEdit.style.display="none";
+    editTextArea.style.resize="none";
 
     cardDiv.appendChild(showButton);
     cardDiv.appendChild(editButton);
     cardDiv.appendChild(questionDiv);
     cardDiv.appendChild(answerDiv);
+    cardDiv.appendChild(confirmEdit);
     questionDiv.appendChild(editTextArea);
 
 
@@ -49,25 +58,37 @@ function saveFlashCard(){
         answerDiv.style.display="none";
     }
 })
-
+/*
 cardDiv.addEventListener("dblclick",function(){
 cardDiv.remove();
-})
+})*/
 
 
 editButton.addEventListener("click",function(){
     editTextArea.placeholder="Write here your new question";
     if(editTextArea.style.display=="none")
-    editTextArea.style.display="block";
-    else editTextArea.style.display="none";
+    {editTextArea.style.display="block";
+    confirmEdit.style.display="block";
+}
+    else {editTextArea.style.display="none";
+    confirmEdit.style.display="none";
+}
+
+confirmEdit.addEventListener("click",function(){
+    //var q =document.getElementById("question");
+    var newText= document.getElementById("newText");
+    questionDiv.innerHTML = newText.value;
+    confirmEdit.style.display="none";
+})
+
 })
 
 cardDiv.setAttribute("style", "background:white; padding:20px 40px; border: 1px solid orange; margin:10px; overflow:hidden;");
-if(document.getElementById("question").value!="")
+
+if(document.getElementById("question").value!="" && document.getElementById("answer").value!="")
     {container3.insertAdjacentElement("beforeend", cardDiv);
     document.getElementById("question").value="";
     document.getElementById("answer").value="";}
 
     
 }
-
